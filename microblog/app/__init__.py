93f8ed73c44f49007e2fb8edc5c654aa2d3c5c5e
YS office LLC <yusuke.sato@ys-office.me>
2021-09-17 19:49:25 +0900

    Added logging mechanism

--- microblog/app/__init__.py
+++ microblog/app/__init__.py
@@ -1,5 +1,5 @@
 import logging
-from logging.handlers import SMTPHandler
+from logging.handlers import SMTPHandler, RotatingFileHandler
 import os
 from flask import Flask
 from config import Config
@@ -30,5 +30,16 @@ if not app.debug:
             credentials=auth, secure=secure)
         mail_handler.setLevel(logging.ERROR)
         app.logger.addHandler(mail_handler)
+    if not os.path.exists('logs'):
+        os.mkdir('logs')
+    file_handler = RotatingFileHandler('logs/microblog.log', maxBytes=10240,
+                                       backupCount=10)
+    file_handler.setFormatter(logging.Formatter(
+        '%(asctime)s %(levelname)s: %(message)s [in %(pathname)s:%(lineno)d]'))
+    file_handler.setLevel(logging.INFO)
+    app.logger.addHandler(file_handler)
+
+    app.logger.setLevel(logging.INFO)
+    app.logger.info('Microblog startup')
 
 from app import routes, models, errors
