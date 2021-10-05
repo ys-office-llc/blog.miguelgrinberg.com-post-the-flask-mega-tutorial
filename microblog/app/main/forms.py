25b83a587aad378a2cb5f89a87de39dd57d3a2ce
YS office LLC <yusuke.sato@ys-office.me>
2021-10-05 21:43:35 +0900

    Add search form

--- microblog/app/main/forms.py
+++ microblog/app/main/forms.py
@@ -4,6 +4,18 @@ from wtforms import StringField, SubmitField, TextAreaField
 from wtforms.validators import ValidationError, DataRequired, Length
 from flask_babel import _, lazy_gettext as _l
 from app.models import User
+from flask import request
+
+
+class SearchForm(FlaskForm):
+    q = StringField(_l('Search'), validators=[DataRequired()])
+
+    def __init__(self, *args, **kwargs):
+        if 'formdata' not in kwargs:
+            kwargs['formdata'] = request.args
+        if 'csrf_enabled' not in kwargs:
+            kwargs['csrf_enabled'] = False
+        super(SearchForm, self).__init__(*args, **kwargs)
 
 
 class EditProfileForm(FlaskForm):
@@ -30,4 +42,3 @@ class EmptyForm(FlaskForm):
 class PostForm(FlaskForm):
     post = TextAreaField(_l('Say something'), validators=[DataRequired()])
     submit = SubmitField(_l('Submit'))
-
