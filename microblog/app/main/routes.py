37ee3c2c7ad1c4531e6dea7e640cdcd4d146ef91
YS office LLC <yusuke.sato@ys-office.me>
2021-10-07 12:46:32 +0900

    Sending a Private Message

--- microblog/app/main/routes.py
+++ microblog/app/main/routes.py
@@ -11,6 +11,8 @@ from app.translate import translate
 from app.main import bp
 from flask import g
 from app.main.forms import SearchForm
+from app.main.forms import MessageForm
+from app.models import Message
 
 
 @bp.before_app_request
@@ -163,3 +165,22 @@ def search():
                            posts=posts,
                            next_url=next_url,
                            prev_url=prev_url)
+
+
+@bp.route('/send_message/<recipient>', methods=['GET', 'POST'])
+@login_required
+def send_message(recipient):
+    user = User.query.filter_by(username=recipient).first_or_404()
+    form = MessageForm()
+    if form.validate_on_submit():
+        msg = Message(author=current_user,
+                      recipient=user,
+                      body=form.message.data)
+        db.session.add(msg)
+        db.session.commit()
+        flash(_('Your message has been sent.'))
+        return redirect(url_for('main.user', username=recipient))
+    return render_template('send_message.html',
+                           title=_('Send Message'),
+                           form=form,
+                           recipient=recipient)
