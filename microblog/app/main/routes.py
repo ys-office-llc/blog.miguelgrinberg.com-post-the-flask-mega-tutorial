d01bc01c4b8897f817f869bf237cb4b0c8237d7f
YS office LLC <yusuke.sato@ys-office.me>
2021-10-16 17:40:30 +0900

    Delivering Notifications to Clients

--- microblog/app/main/routes.py
+++ microblog/app/main/routes.py
@@ -12,7 +12,7 @@ from app.main import bp
 from flask import g
 from app.main.forms import SearchForm
 from app.main.forms import MessageForm
-from app.models import Message
+from app.models import Message, Notification
 
 
 @bp.before_app_request
@@ -177,6 +177,7 @@ def send_message(recipient):
                       recipient=user,
                       body=form.message.data)
         db.session.add(msg)
+        user.add_notification('unread_message_count', user.new_messages())
         db.session.commit()
         flash(_('Your message has been sent.'))
         return redirect(url_for('main.user', username=recipient))
@@ -190,6 +191,7 @@ def send_message(recipient):
 @login_required
 def messages():
     current_user.last_message_read_time = datetime.utcnow()
+    current_user.add_notification('unread_message_count', 0)
     db.session.commit()
     page = request.args.get('page', 1, type=int)
     messages = current_user.messages_received.order_by(
@@ -203,3 +205,16 @@ def messages():
                            messages=messages.items,
                            next_url=next_url,
                            prev_url=prev_url)
+
+
+@bp.route('/notifications')
+@login_required
+def notifications():
+    since = request.args.get('since', 0.0, type=float)
+    notifications = current_user.notifications.filter(
+        Notification.timestamp > since).order_by(Notification.timestamp.asc())
+    return jsonify([{
+        'name': n.name,
+        'data': n.get_data(),
+        'timestamp': n.timestamp
+    } for n in notifications])
--- microblog/app/models.py
+++ microblog/app/models.py
@@ -7,6 +7,8 @@ from werkzeug.security import generate_password_hash, check_password_hash
 import jwt
 from app import db, login
 from app.search import add_to_index, remove_from_index, query_index
+import json
+from time import time
 
 
 class SearchableMixin(object):
@@ -80,6 +82,9 @@ class User(UserMixin, db.Model):
                                         backref='recipient',
                                         lazy='dynamic')
     last_message_read_time = db.Column(db.DateTime)
+    notifications = db.relationship('Notification',
+                                    backref='user',
+                                    lazy='dynamic')
 
     def __repr__(self):
         return '<User {}>'.format(self.username)
@@ -124,6 +129,12 @@ class User(UserMixin, db.Model):
         return Message.query.filter_by(recipient=self).filter(
             Message.timestamp > last_read_time).count()
 
+    def add_notification(self, name, data):
+        self.notifications.filter_by(name=name).delete()
+        n = Notification(name=name, payload_json=json.dumps(data), user=self)
+        db.session.add(n)
+        return n
+
     @staticmethod
     def verify_reset_password_token(token):
         try:
@@ -160,3 +171,14 @@ class Message(db.Model):
 
     def __repr__(self):
         return '<Message {}>'.format(self.body)
+
+
+class Notification(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(128), index=True)
+    user_id = db.Column(db.Integer, db.ForeignKey('user.id'))
+    timestamp = db.Column(db.Float, index=True, default=time)
+    payload_json = db.Column(db.Text)
+
+    def get_data(self):
+        return json.loads(str(self.payload_json))
